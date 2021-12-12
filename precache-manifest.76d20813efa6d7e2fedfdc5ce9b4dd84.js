53d01ca87539ec41e8a9e98f8b0c91e245d95a22
lee6852 <jsjs6852@gmail.com>
2021-12-12 23:45:17 +0900

    Updates

---
+++ precache-manifest.76d20813efa6d7e2fedfdc5ce9b4dd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8419323c4e07b4e2abb764b6d6b116fb",
+    "url": "/git_test/index.html"
+  },
+  {
+    "revision": "ea1d12a37a64209b140d",
+    "url": "/git_test/static/css/main.3f850d22.chunk.css"
+  },
+  {
+    "revision": "470fac99b1672efe392d",
+    "url": "/git_test/static/js/2.948457e8.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/git_test/static/js/2.948457e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1d12a37a64209b140d",
+    "url": "/git_test/static/js/main.4ac2fb59.chunk.js"
+  },
+  {
+    "revision": "1971336b1b4ed3d3f990",
+    "url": "/git_test/static/js/runtime-main.713bfa50.js"
+  }
+]);
